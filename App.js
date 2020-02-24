6b4487ee6a22a1130a600becbe53b523e659ad20
ahmedkamel095 <ahmedkamel095@gmail.com>
2020-02-24 23:49:46 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+
+import PlacesNavigator from "./src/navigation/PlacesNavigator";
+import placesReducer from "./src/store/places-reducer";
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
+  );
+}
